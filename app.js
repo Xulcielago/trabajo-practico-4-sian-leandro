c91856269ac2bdf02b33a375fedaecf6f838d26c
Xulcielago <leandrosian@hotmail.com>
2025-06-26 07:20:27 -0300

    Instalados paquetes nodemon, mysql2 y sequelize, conectada ruta con database.js

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 import express from "express";
+import { startDb } from "./scr/config/database.js";
 
 //Leyendo variables de entorno nativamente
 process.loadEnvFile();
 
 const app = express();
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3001;
 
 
 
-app.listen(PORT, () => { console.log("Servidor listo en el puerto " + PORT, `http://localhost:${PORT}`); });
+app.listen(PORT, async () => { 
+    await startDb();
+    console.log("Servidor listo en el puerto " + PORT, `http://localhost:${PORT}`);
+ });
